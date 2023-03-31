420e5979e6a48a0c2d68747d4ec78b40828616ed
Sergey Kozlov <mne@sergeykozlov.ru>
2023-04-01 01:09:03 +0300

    start Video converter

--- upload/getmytask/index.php
+++ upload/getmytask/index.php
@@ -6,19 +6,27 @@
  * Time: 23:13
  */
 
-include_once ($_SERVER['DOCUMENT_ROOT'] . '/nad/index.php');
-include_once ($_SERVER['DOCUMENT_ROOT'] . '/system/log/log.php');
+require($_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php');
+
+//use VideMe\Datacraft\TM;
+use VideMe\Datacraft\nad;
+
+//use VideMe\Datacraft\log\log;
+use VideMe\Ffmpegconversion\LogConversion;
+use VideMe\Ffmpegconversion\PG_ffmpeg;
+//use VideMe\Datacraft\model\PostgreSQL;
+//use VideMe\Datacraft\index;
 
 //error_reporting(0); // Turn off error reporting
-//error_reporting(E_ALL ^ E_DEPRECATED); // Report all errors
+error_reporting(E_ALL ^ E_DEPRECATED); // Report all errors
 
 $welcome = new NAD();
-$log = new log();
+$log = new LogConversion();
 $userId = $welcome->CookieToUserId();
-
+print_r($userId);
 if (!empty($userId)) {
     $welcome->outputDDBData($log->pgGetMyTask(['user_id' => $userId,
         "limit" => $welcome->setLimit()]));
 } else {
-    header("HTTP/1.0 404 Not Found");
+    //header("HTTP/1.0 404 Not Found");
 }
